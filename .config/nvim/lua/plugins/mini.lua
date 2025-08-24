6364d0ae79434bfdf7a6f27c111a9ff12ade1dac
Richtermnd <m.rustam1305@gmail.com>
2025-08-25 00:49:08 +0300

    remove mini.git

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,7 +3,6 @@ return {
     version = false,
     config = function()
         require("mini.icons").setup()
-        require("mini.git").setup()
         require("mini.move").setup()
         require("mini.pairs").setup()
     end
